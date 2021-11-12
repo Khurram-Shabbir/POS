ead10fc50910c640bd40bbed6ceb71e4809d2387
khurram90000 <khurram90000@gmail.com>
2021-11-13 01:22:51 +0500

    Home > CreateUser view added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,13 @@ namespace POS.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        public IActionResult UpdateUser()
+        {
+            return View();
+        }
     }
 }
